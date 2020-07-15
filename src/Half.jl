05bee4c8173554a9a6344dbf6c33e02598cd8eac
Antara Hebbar <antara.hebbar@gmail.com>
2020-07-15 12:38:49 -0400

    implementation of vhalf to check half

--- src/Half.jl
+++ src/Half.jl
@@ -2,10 +2,50 @@
 include("tools.jl")
 export sv
 
-include("VHalf.jl")
-export vhalf
+include("halfproof.jl")
+export halfproof
 
-function half(m::Int64, s::Int64)
+function vhalf1(m,s,a)
+
+total=m//s
+V,W,Vshr,Wshr = sv(m,s)
+
+lowerproof= 1-(m//s)*(1//(V-2))
+upperproof=(m//s)*(1//(V+1))
+
+if lowerproof>a||upperproof>a
+    return false
+elseif m<=0||s<=0||a<=0
+    return false
+elseif a<1//3
+    return false
+elseif a>1
+    return false
+else
+
+((_, x,), (y,_)) = findend(m,s,a,V)
+
+check1 = (total-x)*(1//(V-1))
+check2= 1-(total-y)*(1//(W-1))
+
+if check1!=a ||check2!=a
+    return false
+else
+
+    if x<=1//2 && V*Vshr>m
+        return true
+    elseif y>=1/2 && W*Wshr>m
+        return true
+    else
+        return false
+end
+
+end
+end
+end
+
+function half(m::Int64, s::Int64, proof::Bool)
+
 if m % s==0
         return "s divides into m, output is 1"
 elseif m < s
@@ -20,16 +60,10 @@ alpha2 = (m//s-1//2)//(V-1)
 if W*Wshr>V*Vshr
     if alpha1<1//3
         a = 1//3
-        output = vhalf(m,s,a)
-        if output
-            return a
-        else
-            return "No output"
-        end
+        return a
     else
         a=alpha1
-        output=vhalf(m,s,a)
-        if output
+        if vhalf1(m,s,a)
             return a
         else
             return "No output"
@@ -39,21 +73,17 @@ if W*Wshr>V*Vshr
 elseif W*Wshr<V*Vshr
     if alpha2<1//3
         a = 1//3
-        output=vhalf(m,s,a)
-        if output
-            return a
-        else
-            return "No output"
-        end
+        return a
     else
         a=alpha2
-        output=vhalf(m,s,a)
-        if output
+        if vhalf1(m,s,a)
             return a
         else
             return "No output"
         end
     end
 end
+
 end
+
 end
