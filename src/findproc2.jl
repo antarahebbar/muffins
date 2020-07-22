a744a3f00d3610e8e32bc62b7428d1ef31727739
Antara Hebbar <antara.hebbar@gmail.com>
2020-07-22 18:24:12 -0400

    implementation of findproc

--- src/findproc2.jl
+++ src/findproc2.jl
@@ -1,4 +1,181 @@
-function findproc(muffins::Int64, students::Int64, alpha::Int64)
+#jason's findproc
+
+using JuMP
+using Cbc
+
+include("Formatj.jl")
+export printf, printfT, printHeader, findlast, center
+
+B = []
+memo = Dict()
+
+# Determines procedure with minimum piece size alpha -- Work in progress
+function findproc(m::Int64, s::Int64, alpha::Rational{Int64}; output::Int64=1)
+    output > 0 && printHeader(center("FIND PROCEDURE"))
+
+    # Clear globals
+    global B = []
+    global memo = Dict()
+
+    c = numerator(alpha)
+    d = denominator(alpha)
+    b = lcm(s, d)
+
+    a = Int64(b * c / d)
+    B = [x for x in a:(b-a)]
+
+    M = []
+    S = []
+    solutions = []
+
+    try
+        # Determine vector sets
+        L = b - 2a + 1
+        V = Int64(ceil(2m/s))
+        T = Int64(b * m / s)
+        M = vectorize(f(L, b, 2))
+        S = vectorize(unionF(f(L, T, V), f(L, T, V-1)))
+
+        # Initialize model
+        model = Model(Cbc.Optimizer)
+        set_optimizer_attribute(model, "logLevel", 0)
+        set_optimizer_attribute(model, "allowableGap", 1e-5)
+        set_optimizer_attribute(model, "cuts", "off")
+
+        @variable(model, 0 <= x[i=1:length(M)], Int)
+        @variable(model, 0 <= y[i=1:length(S)], Int)
+
+        for k=1:L
+            @constraint(model, sum(x .* getindex.(M, k)) == sum(y .* getindex.(S, k)))
+        end
+
+        @constraint(model, sum(x) == m)
+        @constraint(model, sum(y) == s)
+
+        # Solve for natural number solutions
+        for i=1:length(M)
+            @objective(model, Min, x[i])
+            optimize!(model)
+            append!(solutions, [[value.(x), value.(y)]])
+
+            @objective(model, Max, x[i])
+            optimize!(model)
+            append!(solutions, [[value.(x), value.(y)]])
+        end
+
+        solutions = unique(solutions)
+    catch
+        # TODO -- implement error reporting
+        Nothing
+    end
+
+    alpha = formatFrac(alpha)
+
+    # Display solutions
+    if output > 0
+        printHeader("OVERVIEW")
+        printfT("Goal",
+                "Find procedures for dividing $m muffins among $s students where $alpha is the smallest piece size")
+        printfT("Note",
+                "Let the common denominator be $b")
+
+        printHeader("SOLUTIONS")
+    end
+
+    # Exit if no solutions
+    if length(solutions) == 0
+        if output > 0
+            printf("No solutions for muffins($m, $s, $alpha", line=true)
+            printEnd()
+        end
+        return Nothing
+    end
+
+    # Output each solution
+    if output > 0
+        for k=1:length(solutions)
+            x = solutions[k][1]
+            y = solutions[k][2]
+
+            divide = ["Divide $(Int64(x[i])) muffins {$(unpack(M[i]))}" for i=1:length(M) if x[i] > 0]
+            give = ["Give $(Int64(y[i])) students {$(unpack(S[i]))}" for i=1:length(S) if y[i] > 0]
+
+            printfT("Solution $k",
+                    divide...,
+                    give...)
+        end
+        printEnd()
+    end
+
+    #return [b, B, M, S, solutions]
+end
+
+# Helper function for findproc -- "vectorizes" arrays in set S based on B
+function vectorize(S)
+    S == Nothing ? Nothing : sort([[count(i->(i==x), s) for x in B] for s in S], rev=true)
+end
+
+# Helper function for findproc -- "unpacks" vectors into strings based on B
+function unpack(V)
+    join(vcat(map(i->repeat([B[i]], V[i]), 1:length(V))...), ", ")
+end
+
+# Helper function for findproc -- "recursively" determines k-size subsets of B[1:i] that sum to T
+function f(i, T, k)
+    # Base cases
+    if i >= 1 && T == 0 && k == 0
+        Set()
+    elseif T == 0 && k >= 1
+        Nothing
+    elseif i == 0 && T >= 1
+        Nothing
+    elseif T >= 1 && k == 0
+        Nothing
+    elseif T <= -1
+        Nothing
+    elseif i == 1
+        T == k * B[1] ? Set([repeat([B[1]], k)]) : Nothing
+    elseif i >= 1 && k == 1
+        index = findall(x->x==T, B[1:i])
+        length(index) > 0 ? Set([[B[index[1]]]]) : Nothing
+    # Recursive case with memoization
+    else
+        if haskey(memo, (i, T, k))
+            get(memo, (i, T, k), 0)
+        else
+            set = unionF( f(i-1, T, k), mapCat(f(i, T-B[i], k-1), [B[i]]) )
+            get!(memo, (i, T, k), set)
+        end
+    end
+end
+
+# Helper function for f -- set unions with Nothing
+function unionF(x, y)
+    if x == Nothing && y == Nothing
+        Nothing
+    elseif x == Nothing
+        y
+    elseif y == Nothing
+        x
+    else
+        union(x, y)
+    end
+end
+
+# Helper function for f -- concatenate e with each element in set S
+function mapCat(S, e)
+    S == Nothing ? Nothing : Set([vcat(s, e) for s in S])
+end
+
+
+
+
+
+
+
+
+
+#=function findproc(muffins::Int64, students::Int64, alpha::Int64)
 
 
 
@@ -58,7 +235,7 @@ exclude = solve(B, n, T-B[n-1], j-1)
 
 println(include, exclude)
 end
-#=if sum == 0
+if sum == 0
     return true
 elseif n<=0 && sum!=0
     return false
---
+++ src/minipackage.jl
@@ -0,0 +1,123 @@
+#problems with cases (16,13)
+include("findproc2.jl")
+export findproc, vectorize, unpack, f, unionF, mapCat
+
+include("FCBound.jl")
+export fc
+
+include("Half.jl")
+export half, vhalf1
+
+include("halfproof.jl")
+export halfproof
+
+include("tools.jl")
+export sv, findend
+
+include("Int.jl")
+export vint1, int
+
+include("intproof.jl")
+export intproof
+
+include("formatj.jl")
+export toFrac
+
+
+#given m,s, program will output min of half, int, and fc
+function mini(m::Int64, s::Int64)
+
+alpha_fc = fc(m,s)
+
+alpha_half = half(m,s)
+
+alpha_int = int(m,s)
+
+
+verify = true
+
+if m%s==0
+    return "The # of students divides into the # of muffins, so the answer is 1."
+    verify=false
+elseif m<s||m<=0||s<=0
+    return "M has to be greater than s, and both values should be >0."
+    verify=false
+else
+
+#checking if any method did not get conclusive alpha, 2 signifies the method failed
+alpha_int = alpha_int== -1 ? alpha_int = "2" : alpha_int=alpha_int
+alpha_half = alpha_half== -1 ? alpha_half = "2" : alpha_half=alpha_half
+alpha_fc = alpha_fc== -1 ? alpha_fc = "2" : alpha_fc=alpha_fc
+
+
+#converting to rational integers
+fracfc = toFrac(alpha_fc)
+fracint = toFrac(alpha_int)
+frachalf = toFrac(alpha_half)
+
+#finding min of three methods
+alpha= min(fracfc, frachalf, fracint)
+den = lcm(s, denominator(alpha))
+alphaS=fracstring(alpha, den)
+
+if alpha==2//1
+    return "FC, Half, and INT failed. Minipackage does not work for muffins($m,$s)."
+    verify = false
+
+else
+
+printHeader("DERIVED ALPHA:")
+printf("Mini package found that muffins($m,$s) <= $alphaS")
+
+printHeader("METHOD USED: ")
+
+methodhalf=false
+methodint=false
+
+#if more than one method was used
+if fracfc==alpha&&frachalf==alpha
+    printf("The FC and Half methods can be used to derive alpha.")
+elseif fracfc==alpha&&fracint==alpha
+    printf("The FC and Int methods can be used to derive alpha.")
+elseif frachalf==alpha&&fracint==alpha
+    printf("The Int and Half methods can be used to derive alpha.")
+    methodhalf=true
+    methodint=true
+elseif frachalf==alpha&&fracint==alpha&&fracfc==alpha
+    printf("Int, Half, and FC all outputted the same alpha. Any of these methods can be used.")
+else
+
+#if only one method was used
+if fracfc==alpha
+    printf("The Floor Ceiling method was used to derive alpha.")
+elseif alpha==fracint
+    methodint=true
+    printf("The INT method was used to derive alpha.")
+elseif alpha==frachalf
+    methodhalf=true
+    printf("The Half method was used to derive alpha")
+else
+    printf("Mini package failed, cannot find a method used to derive alpha")
+end
+end
+
+
+#outputting a proof for half and int
+printHeader("PROOF: ")
+if methodhalf
+    return halfproof(m,s,alpha), findproc(m,s,alpha)
+
+elseif methodint
+    return intproof(m,s,alpha), findproc(m,s,alpha)
+
+else
+    print("Since Floor Ceiling can be used to derive alpha, no proof is necessary."), findproc(m,s,alpha)
+
+end
+
+end
+
+
+end
+
+end
